c3c789bc351edc1160e1f3e330af4d5e2f3289c3
miguelangelarmeromunoz8-jpg <miguelangelarmeromunoz8@gmail.com>
2026-04-15 09:18:22 -0500

    Actualizar ConexionBD.java

--- recu/src/modelo/conexion/ConexionBD.java
+++ recu/src/modelo/conexion/ConexionBD.java
@@ -6,14 +6,11 @@ import controlador.Coordinador;
 import modelo.dto.PersonaDTO;
 
 public class ConexionBD {
-
-	// Simulación de base de datos usando un mapa
 	
 	public static HashMap<String, PersonaDTO> personasMap;
 	private Coordinador Coordinador;
 
 	public ConexionBD() {
-		// Inicializamos el contenedor de los datos
 		
 		personasMap = new HashMap<String, PersonaDTO>();
 	}
